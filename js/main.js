dd689df398c7783919a7ed6843a8a8639f6595da
Cecilia Cid <ccid.montero@gmail.com>
2018-06-27 21:23:52 -0400

    Cargado de api, css y nav

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
-/* Holiii acá va tu código también */
+let bipUsuario = document.getElementById("bipUsuario");
+let saldoBip = document.querySelector("#saldo");
+let fechaBip = document.querySelector("#fechaSaldo");
+let botonCargar = document.getElementById("cargarDatos");
+
+/////////////////////////////////////////////////////////////////
+
+
+function cargar (){
+    //let bip = bipUsuario.value;
+    //console.log(bip)
+    //bipUsuario.value = "";
+
+    fetch(`http://www.psep.cl/api/Bip.php?&numberBip=666`)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data["Estado de contrato"]);
+        //renderInfo(data);
+    })
+}
+
+
+/*const renderInfo = data => {
+    saldoBip.innerHTML = data.Saldo;
+    fechaBip.innerHTML = data.Fecha;
+}
+*/
